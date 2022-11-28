88021fdf7d71469b346bc4eccea6e29f26243e13
rizal57 <fahrurrzl5798@gmail.com>
2022-11-28 10:02:41 +0700

    [IMP] Improve welcome view

---
+++ resources/views/components/button-primary.blade.php
@@ -0,0 +1,3 @@
+<button  class="bg-blue-500 shadow-lg shadow-blue-500/30 active:bg-blue-700 text-white font-semibold py-2 px-4 rounded-full text-sm hover:bg-blue-600 transition ease-out duration-300">
+    {{ $slot }}
+</button>
