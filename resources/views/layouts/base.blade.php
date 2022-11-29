a3bede9bf4369920f9260be9a700d902d12b61b1
rizal57 <fahrurrzl5798@gmail.com>
2022-11-29 21:32:54 +0700

    [IMP] Improve alpine js cdn to local

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -22,7 +22,6 @@
 
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     </head>
 
     <body>
