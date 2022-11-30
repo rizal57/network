b826bf6a2f7320388bf92cc2fc9fbd3f5c95f9a6
rizal57 <fahrurrzl5798@gmail.com>
2022-11-30 07:48:54 +0700

    [FIX] fix update and delete statuses in dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -16,6 +16,32 @@ class Dashboard extends Component
         return view('livewire.dashboard');
     }
 
+    public function deleteStatus($id)
+    {
+        Status::destroy($id);
+    }
+
+    public function editStatus($id)
+    {
+        $status = Status::find($id);
+        $this->body = $status->body;
+        $this->status_id = $id;
+    }
+
+    public function updateStatus($id)
+    {
+        Status::where('id', $id)->update([
+            'body' => $this->body
+        ]);
+
+        $this->status_id = NULL;
+    }
+
+    public function loadMore()
+    {
+        $this->limit += 10;
+    }
+
     public function like($id)
     {
         $like = Like::where('status_id', $id)->where('user_id', auth()->user()->id)->first();
